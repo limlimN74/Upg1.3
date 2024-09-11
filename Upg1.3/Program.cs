adebb8a3f7f7c0e41f6454298052c4940cc95775
lini07003 <lini07003@E-PW05C37N>
2024-09-11 12:38:05 +0200

    Add project files.

---
+++ Upg1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace upg1_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\ ");
+            Console.WriteLine("/______\\");
+            //Denna upgift var lite klurig//
+        }
+    }
+}
